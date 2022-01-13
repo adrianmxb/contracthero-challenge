c42e09919cbd874e145ae6ca2d1e3a76c5af3ae7
Adrian Busse <git@adrianmxb.com>
2022-01-13 18:41:30 +0100

    feat: pdf analyzer controller and service

--- server/src/controllers/analyze.ts
+++ server/src/controllers/analyze.ts
@@ -1,11 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
+import { PDFService } from "../services/PDFService";
 
 
 export default () => {
     const router = Router();
+    const upload = multer();
 
-    router.post("/", async() => {
+    const pdfService = new PDFService();
 
+    router.post("/", upload.single('file'), async (req, res) => {
+        if(!req.file) {
+            return res.send(400).send("no valid file uploaded");
+        }
+
+        const parsedText = await pdfService.parseFile(req.file.buffer);
+        res.send({
+            parsedContent: parsedText,
+        });
     });
 
     return router;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import { registerContollers } from './controllers';
 
 const app = express();
 app.use(cors());
+
+registerContollers(app);
 
 app.listen(3001, () => {
     console.log("server running strong on port 3001!");
