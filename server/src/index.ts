568ecaa2c9f6b581b6b38d58ee30913cdf40db33
Adrian Busse <git@adrianmxb.com>
2022-01-13 18:22:28 +0100

    chore: change default port of server to 3001

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,6 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
-app.listen(3000, () => {
-    console.log("server running strong on port 3000!");
+app.listen(3001, () => {
+    console.log("server running strong on port 3001!");
 })
